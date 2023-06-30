471ca5f4a7ddc6178b8bf8ed2559d6fd38431d0d
Michael Berthel <michael.berthel@googlemail.com>
2023-06-30 13:31:19 +0200

    modified:   app.py 	new file:   docs/AKQ Sonstiges.pdf 	new file:   docs/AKQ Standard.pdf 	new file:   "docs/Abbuchungen \342\200\236ohne Daten\342\200\234.pdf" 	new file:   "docs/Abk\303\274rzungen.pdf" 	new file:   "docs/Adress\303\244nderungen.pdf" 	new file:   docs/Apollon.pdf 	new file:   docs/Automatischer Mail-Vorschlag.pdf 	new file:   docs/BTR Sonstiges.pdf 	new file:   docs/Bankbandkontrolle.pdf 	new file:   "docs/Bankverbindungs\303\244nderungen.pdf" 	new file:   docs/Bounceback (WBB, GBB).pdf 	new file:   docs/Brief- und Mail-Vorlagen.pdf 	new file:   docs/CC PA Anfragen.pdf 	new file:   docs/DL-RUECK.pdf 	new file:   docs/Das Gute Geschenk (DGG).pdf 	new file:   docs/Datenpflege.pdf 	new file:   docs/Dublettenbereinigung.pdf 	new file:   docs/Einmalspenden Sonderzahlungen.pdf 	new file:   docs/Einmalspenden.pdf 	new file:   docs/Feldanfragen.pdf 	new file:   "docs/Freie Spende (regelm\303\244\303\237ig).pdf" 	new file:   "docs/Fr\303\274hk\303\274ndiger.pdf" 	new file:   "docs/J\303\244hrliche Zusatzspende.pdf" 	new file:   docs/KUEND-ANDERE.pdf 	new file:   docs/KUEND_KIRE.pdf 	new file:   docs/Keine Patengruppenreisen mehr!.pdf 	new file:   docs/Kindheits-Retter.pdf 	new file:   "docs/K\303\274ndigung-PA.pdf" 	new file:   docs/Nachrichtenweiterleitung.pdf 	new file:   "docs/PA - Direkt\303\274bernahmen.pdf" 	new file:   docs/PA - Zuord.pdf 	new file:   "docs/PA - \303\234bernahmen.pdf" 	deleted:    docs/PDF-LangChain.jpg 	new file:   docs/PSS AKQ.pdf 	new file:   docs/PSS BTR.pdf 	new file:   docs/PSS-Sortieren - nur 2nd!.pdf 	new file:   docs/Prioliste.pdf 	new file:   "docs/Produkt\303\274bersicht.pdf" 	new file:   docs/Projektpatenschaften.pdf 	new file:   docs/RLS Aufgaben.pdf 	new file:   docs/RPL.pdf 	new file:   docs/Recherche.pdf 	new file:   "docs/R\303\274cklastschriften (RLS) und RLS Telefonie.pdf" 	new file:   docs/SONST. ADMINISTRATION.pdf 	new file:   docs/Sonderspenden Kind_Projekt.pdf 	new file:   docs/Terminablage.pdf 	new file:   docs/Transitions.pdf 	new file:   "docs/Vorlagen f\303\274r Leerbriefe und Leer-E-Mails.pdf" 	new file:   docs/Wer kann was_.pdf 	new file:   docs/WhatsApp-Chat.pdf

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+import glob
+import os
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
 
-def get_pdf_text(pdf_docs):
+dir_to_read ="docs"
+pdf_files = glob.glob(os.path.join(dir_to_read,'*.pdf'))
+
+def get_pdf_text(pdf_files):
     text = ""
-    for pdf in pdf_docs:
+    for pdf in pdf_files:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
@@ -66,28 +70,30 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+    
+    st.set_page_config(page_title="Chatte mit dem Wissensmanagement von WorldVision",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
+
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    
+    st.header("Chatte mit dem Wissensmanagement von WorldVision :books:")
+    user_question = st.text_input("Was möchtest Du wissen:")
     if user_question:
         handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
+        st.subheader("Dokumentenbibliothek")
+        
+        if st.button("Verarbeiten"):
+            with st.spinner("Dokumente werden verarbeitet...."):
                 # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+                raw_text = get_pdf_text(pdf_files)
 
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
@@ -99,6 +105,6 @@ def main():
                 st.session_state.conversation = get_conversation_chain(
                     vectorstore)
 
-
+    
 if __name__ == '__main__':
     main()
